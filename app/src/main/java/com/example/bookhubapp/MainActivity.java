b402ad1a89732d52d41a9dec099926e6f87d5a0e
ToTuanAn <73640290+ToTuanAn@users.noreply.github.com>
2022-07-01 18:54:28 +0700

    Intro base finished

--- app/src/main/java/com/example/bookhubapp/MainActivity.java
+++ app/src/main/java/com/example/bookhubapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.bookhubapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout startBin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBin = findViewById(R.id.startBin);
+        startBin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
